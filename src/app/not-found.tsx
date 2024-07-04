85ff100eb81bac3b4c46c8361a4373ca7558b3a6
junghyunwoo02 <jungilbung@naver.com>
2024-07-04 22:28:39 +0900

    feat: 404페이지 텍스트 변경

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ const NotFoundPage = () => {
       />
       <h2 className="font-bold mt-4 text-lg">⚠️ 앗! 여긴 어디죠?</h2>
       <p className="text-center mt-4">
-        아직 지구상에 존재하지 않는 주소를 미리 입력하셨군요!
+        아직 지구상에 존재하지 않는 주소를 최초로 입력하셨군요!
         <br />
         피카츄가 말해주길, 아니면 요청하신 페이지의 주소가 변경, 삭제되어 찾을
         수 없다네요. <br />
